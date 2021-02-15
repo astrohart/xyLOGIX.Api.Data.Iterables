bc4c676f9c4a4c6a68b59a608a9cb3e328544157
Brian Hart <brian.hart@xylogix.net>
2021-02-15 00:46:20 -0500

    Create IterableBase class

---
+++ xyLOGIX.Api.Data.Iterables.Interfaces/IIterable.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace xyLOGIX.Api.Data.Iterables.Interfaces
+{
+    /// <summary>
+    /// Defines the public-exposed methods and properties of an iterable object
+    /// (the API data version of
+    /// <see
+    ///     cref="T:System.Collections.Generic.IEnumerable{T}" />
+    /// . So why make this
+    /// separate interface/object tree? Because you need to then bolt this up to
+    /// a custom implementation that does not necessarily need to do all the
+    /// things an IEnumerable does, given the unique nature of paged API data.
+    /// </summary>
+    public interface IIterable<out T> : IEnumerable<T> where T : class { }
+}
---
+++ xyLOGIX.Api.Data.Iterables/IterableBase.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using xyLOGIX.Api.Data.Iterables.Interfaces;
+
+namespace xyLOGIX.Api.Data.Iterables
+{
+    public abstract class IterableBase<T> : IIterable<T> where T : class
+    {
+        /// <summary>Returns an enumerator that iterates through the collection.</summary>
+        /// <returns>An enumerator that can be used to iterate through the collection.</returns>
+        public abstract IEnumerator<T> GetEnumerator();
+
+        /// <summary>Returns an enumerator that iterates through a collection.</summary>
+        /// <returns>An <see cref="T:System.Collections.IEnumerator" /> object that can be used to iterate through the collection.</returns>
+        IEnumerator IEnumerable.GetEnumerator()
+            => GetEnumerator();
+    }
+}
