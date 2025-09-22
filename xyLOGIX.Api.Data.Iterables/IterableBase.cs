d3aaa36fdd6c96e85d2e891829da72d0ee4590b3
Brian Hart <brian.hart@xylogix.net>
2025-09-22 00:18:50 -0600

    Update IterableBase.cs
    - Replacing all occurrences of `object sender` and `EventArgs e`, respectively, are prefaced with `[NotLogged]`.  This is being done so as to ensure that the value(s) of these parameter(s) do not get written to PostSharp log file(s).  Also updated all auto-property(ies) to make sure their getters and setters are all prefixed with the `[DebuggerStepThrough]` attribute.

--- xyLOGIX.Api.Data.Iterables/IterableBase.cs
+++ xyLOGIX.Api.Data.Iterables/IterableBase.cs
@@ -1,4 +1,5 @@
-﻿using JetBrains.Annotations;
+﻿using System.Diagnostics;
+using JetBrains.Annotations;
 using PostSharp.Patterns.Diagnostics;
 using PostSharp.Patterns.Model;
 using PostSharp.Patterns.Threading;
